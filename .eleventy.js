9d61046a9ad670774e3e431cb858d842474be789
Simon Cox <90435+simoncox@users.noreply.github.com>
2023-08-05 19:32:18 +0100

    Removed superflous passtrhoughs in config

--- .eleventy.js
+++ .eleventy.js
@@ -5,9 +5,7 @@ const CleanCSS = require("clean-css"); // CSS minification
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("cssmin", function(code) { return new CleanCSS({}).minify(code).styles;}); // CSS minification
   eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy("./src/BingSiteAuth.xml");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("./src/humans.txt");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
   eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
   eleventyConfig.addPassthroughCopy("./src/_redirects");
